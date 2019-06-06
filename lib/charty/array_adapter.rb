b4dc7a54def231fe248c7837d4c485d3cbada5b3
284km <k.furuhashi10@gmail.com>
2019-06-06 15:50:48 +0900

    bundle gem -t minitest charty-array_adapter

---
+++ lib/charty/array_adapter.rb
@@ -0,0 +1,8 @@
+require "charty/array_adapter/version"
+
+module Charty
+  module ArrayAdapter
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/charty/array_adapter/version.rb
@@ -0,0 +1,5 @@
+module Charty
+  module ArrayAdapter
+    VERSION = "0.1.0"
+  end
+end
