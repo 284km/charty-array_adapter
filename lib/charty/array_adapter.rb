b167b95877c468639cac3a6bd005961c3acb0bfa
284km <k.furuhashi10@gmail.com>
2019-06-06 16:24:17 +0900

    Experimental implementation

--- lib/charty/array_adapter.rb
+++ lib/charty/array_adapter.rb
@@ -1,4 +1,6 @@
 require "charty/array_adapter/version"
+require "charty/plotter_adapter"
+require "charty/plotter"
 
 module Charty
   module ArrayAdapter
@@ -6,3 +8,29 @@ module Charty
     # Your code goes here...
   end
 end
+
+class Array
+  def plotter
+    @plotter_adapter
+  end
+  def plotter=(adapter_name)
+    @adapter_name = adapter_name
+    @plotter_adapter =  Charty::PlotterAdapter.create(adapter_name)
+  end
+  def to_bar(x, y, **args, &block)
+    seriesx, seriesy = *self
+    xrange = (seriesx.min)..(seriesx.max)
+    yrange = (seriesy.min)..(seriesy.max)
+    bar = bar do
+      series seriesx, seriesy
+      range x: xrange, y: yrange
+      xlabel x
+      ylabel y
+    end
+  end
+  def bar(**args, &block)
+    context = Charty::RenderContext.new :bar, **args, &block
+    context.apply(@plotter_adapter)
+  end
+end
+
